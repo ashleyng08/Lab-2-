5d9d353878af2072ae1539c1cd9ae5f2f9e4803d
Ashley <ashleyanj08.25@ichat.sp.edu.sg>
2026-05-05 15:14:50 +0800

    added return values

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,11 @@
 print ("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def display_main_menu():
+    print("Enter some numbers separated by commas(e.g.5,67,32)")
+display_main_menu()
+def get_user_input(): 
+    number = input()
+    numlist = number.split(",")
+    print(numlist)
+get_user_input()
+
--- bmi.py
+++ bmi.py
@@ -2,13 +2,15 @@ def calculate_bmi(height, weight):
     print("Height="+str(height))
     print("Weight="+str(weight))
     bmi=weight/(height*height)
-    print("bmi="+str(bmi))
-    return bmi 
-bmi=calculate_bmi(weight=57,height=1.73)
-if bmi<18.5:
-    print("Under Weight")
-elif 18.5<=bmi<=25.0:
-    print("Normal Weight")
-else:
-    print("Overweight")
+    print(bmi)
 
+    if bmi<18.5:
+        print("Under Weight")
+        return -1
+    elif bmi>25.0:
+        print("Over Weight")
+        return 0
+    else:
+        print("Normal Weight")
+        return 1 
+calculate_bmi(weight=57,height=1.73)
